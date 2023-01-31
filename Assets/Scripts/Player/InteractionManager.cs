83147599cce119ddd79155c2f02f516032b4398e
daniel-zuluaga <daniel.zuluaga007@gmail.com>
2023-01-30 21:33:43 -0500

    start by adding the game inventory where the items you collect will be stored

--- Assets/Scripts/Player/InteractionManager.cs
+++ Assets/Scripts/Player/InteractionManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using TMPro;
 
 public class InteractionManager : MonoBehaviour
@@ -12,6 +13,7 @@ public class InteractionManager : MonoBehaviour
 
     private GameObject curInteractGameObject;
     private IInteractable curIntearctable;
+    public GameObject objectTextInteractable;
 
     public TextMeshProUGUI promptText;
     private Camera cam;
@@ -19,16 +21,52 @@ public class InteractionManager : MonoBehaviour
     private void Start()
     {
         cam = Camera.main;
+        objectTextInteractable.SetActive(false);
     }
 
     private void Update()
     {
-        if(Time.time - lastCheckTime > checkRate)
+        if (Time.time - lastCheckTime > checkRate)
         {
             lastCheckTime = Time.time;
+
+            Ray ray = cam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, maxCheckDistane, layerMask))
+            {
+                if (hit.collider.gameObject != curInteractGameObject)
+                {
+                    curInteractGameObject = hit.collider.gameObject;
+                    curIntearctable = hit.collider.GetComponent<IInteractable>();
+                    SetPromptText();
+                }
+            }
+            else
+            {
+                curInteractGameObject = null;
+                curIntearctable = null;
+                objectTextInteractable.SetActive(false);
+            }
         }
     }
 
+    void SetPromptText()
+    {
+        objectTextInteractable.SetActive(true);
+        promptText.text = string.Format("<b>[E]</b> {0}", curIntearctable.GetInteractPrompt());
+    }
+
+    public void OnInteractInput(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started && curIntearctable != null)
+        {
+            curIntearctable.OnInteract();
+            curInteractGameObject = null;
+            curIntearctable = null;
+            objectTextInteractable.gameObject.SetActive(false);
+        }
+
+    }
 }
 
 public interface IInteractable
